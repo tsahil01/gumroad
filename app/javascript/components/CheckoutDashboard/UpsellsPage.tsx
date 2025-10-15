79571cb5279ef78f206cd3f309799caff1c40d56
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-15 21:58:53 +0530

    use tailwind classes more

--- app/javascript/components/CheckoutDashboard/UpsellsPage.tsx
+++ app/javascript/components/CheckoutDashboard/UpsellsPage.tsx
@@ -929,7 +929,7 @@ const Form = ({
         </form>
         <CheckoutPreview cartItem={previewCartItem}>
           <dialog
-            className="fixed top-1/2 left-1/2 z-20 flex w-fit max-w-[43.75rem] min-w-[20rem] -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded border border-border bg-background p-8 text-foreground shadow-[0.5rem_0.5rem_0_rgb(0_0_0)] backdrop:bg-black/80 dark:shadow-none [&:not([open])]:hidden"
+            className="fixed top-1/2 left-1/2 z-20 flex w-fit max-w-[43.75rem] min-w-80 -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded border border-border bg-background p-8 text-foreground shadow-md backdrop:bg-black/80 dark:shadow-none [&:not([open])]:hidden"
             open
             aria-labelledby={`${uid}preview`}
           >
